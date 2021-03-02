e2cc9653dcd79287c35bae62b81218167af53487
LeonAvancini <avancinileon@gmail.com>
2021-03-02 11:25:59 -0300

    Bg - línea agregada para separar la tabla de jugadores después de completar todas las coordenadas de los barcos

--- Battleship.java
+++ Battleship.java
@@ -13,6 +13,8 @@ public class Battleship {
         String[][] tableSecondPlayerUi = new String[5][5];
         int firstPlayerScore = 0;
         int secondPlayerScore = 0;
+        int[] separateLines = new int[20];
+
 
         System.out.println("Welcome to Battleship game!");
 
@@ -25,15 +27,18 @@ public class Battleship {
         tableFirstPlayer = BattleshipGameMethods.InsertShipsOnTable(tableFirstPlayer, firstPlayerName);
         System.out.println("This is " + firstPlayerName + " Table:");
         BattleshipGameMethods.Print2D(tableFirstPlayer);
-        int[] separate = new int[10];
 
-        for (int test: separate) {
-            System.out.println("");
+        for (int space: separateLines) {
+            System.out.println("-");
         }
 
         tableSecondPlayer = BattleshipGameMethods.InsertShipsOnTable(tableSecondPlayer, secondPlayerName);
         System.out.println("This is " + secondPlayerName + " Table:");
         BattleshipGameMethods.Print2D(tableSecondPlayer);
+
+        for (int space: separateLines) {
+            System.out.println("-");
+        }
 
         while (true) {
 
